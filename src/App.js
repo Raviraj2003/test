3bad35d2b53bf760fc127ab365310d25fa7cb9d9
Raviraj Mohite <150107804+Raviraj2003@users.noreply.github.com>
2025-04-11 02:13:47 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div id='name'>
+          <h1>HI I AM RAVIRAJ MOHITE </h1>
+        </div>
+        <div id='info'>
+          
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
